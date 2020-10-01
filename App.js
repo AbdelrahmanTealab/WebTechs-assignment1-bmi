92f2221d764355260f799be4b4fea2edc23ad9a9
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2020-10-01 12:06:53 -0400

    added switch toggle button to switch from kg/cm to lbs/in

--- App.js
+++ App.js
@@ -1,55 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Switch, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
   const [weight, onChangeWeight] = useState(0)
   const [height, onChangeHeight] = useState(0)
   var bmi = useState(calculateBMI(weight, height))
-  var isMetric = false;
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
-  function calculateBMI(weight, height){
-    if (isMetric){
-      bmi = (weight/height/height)*10000
+  function calculateBMI(weight, height) {
+    if (!isEnabled) {
+      bmi = (weight / height / height) * 10000
     }
-    else{
-      bmi = (703*weight/height**2)
+    else {
+      bmi = (703 * weight / height ** 2)
     }
     return bmi.toFixed(1);
+  }
+
+  function unitWeight(isMetric) {
+    if (!isEnabled) {
+      return "kg"
+    }
+    else {
+      return "lbs"
+    }
+  }
+  function unitHeight(isMetric) {
+    if (!isEnabled) {
+      return "cm"
+    }
+    else {
+      return "in"
+    }
   }
 
   return (
     <View style={styles.container}>
 
       <View style={styles.rowView}>
-        <Text style={styles.textView}>weight: </Text>
-        <TextInput
-          style={styles.textField}
-          onChangeText = {text=>onChangeWeight(text)}
-          value={weight}
-          keyboardType='numeric'
-        />
-        <Text style={styles.unitStyle}>KG</Text>
+        <Text style={styles.buttonText}>kg/cm</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#aa99aa" }}
+          thumbColor={isEnabled ? "#fffadd" : "#fffadd"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled} />
+        <Text style={styles.buttonText}>lbs/in</Text>
       </View>
 
       <View style={styles.rowView}>
+        <Text style={styles.textView}>weight: </Text>
+        <TextInput
+          style={styles.textField}
+          onChangeText={text => onChangeWeight(text)}
+          value={weight}
+          keyboardType='numeric'
+        />
+        <Text style={styles.unitStyle}>{unitWeight(isEnabled)}</Text>
+      </View>
 
+      <View style={styles.rowView}>
         <Text style={styles.textView}>height: </Text>
         <TextInput
           style={styles.textField}
-          onChangeText = {text=>onChangeHeight(text)}
+          onChangeText={text => onChangeHeight(text)}
           value={height}
           keyboardType='numeric'
         />
-        <Text  style={styles.unitStyle}>CM</Text>
+        <Text style={styles.unitStyle}>{unitHeight(isEnabled)}</Text>
       </View>
 
       <View style={styles.rowView}>
         <Text style={styles.textView}>BMI: </Text>
-        <Text style={styles.bmiView}>{calculateBMI(weight,height)}</Text>
+        <Text style={styles.bmiView}>{calculateBMI(weight, height)}</Text>
       </View>
-      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -85,11 +113,7 @@ const styles = StyleSheet.create({
     color: '#fffadd',
     fontFamily: 'Avenir'
   },
-  buttonText: {
-    fontSize: 40,
-    color: 'white',
-    fontFamily: 'Avenir'
-  },
+
   bmiView: {
     fontSize: 60,
     color: 'white',
@@ -97,8 +121,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontFamily: 'Avenir'
   },
-  unitStyle:{
+  unitStyle: {
     color: 'white',
     fontFamily: 'Avenir'
-  }
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: '#fffadd',
+    fontFamily: 'Avenir',
+    alignContent: "center",
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+
 });
